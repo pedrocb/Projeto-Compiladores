657879a887e60ca15eda454efb87dbc156b87546
GGraca <ggracase@gmail.com>
2016-03-28 16:19:57 +0100

    Added comments to the previous functions

--- tree.h
+++ tree.h
@@ -5,4 +5,14 @@ typedef struct node_{
 }*node;
 
 void print_tree(node n, int depth);
+/*
+node n 					-> where to start
+int depth 			-> aux var for the prints ("..")
+*/
+
 node add_to_tree(char *label, int n_children, ...);
+/*
+char *label 		-> label to name the new node
+int n_children 	-> number of aux arguments
+...							-> list of (node) arguments to add as children of the new node
+*/
